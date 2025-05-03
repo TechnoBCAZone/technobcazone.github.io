21b028fcf4657ab3d115c795367953bc8bdf87e3
TechnoBCAZone <arspim22087088076@gmail.com>
2025-05-03 21:30:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
-# config.py
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 db_config = {
-    'host': "sql12.freesqldatabase.com",
-    'user': "sql12776701",
-    'password': "your_actual_password",  # Replace with the correct password
-    'database': "sql12776701"
+    'host': os.getenv("DB_HOST"),
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASS"),
+    'database': os.getenv("DB_NAME")
 }
